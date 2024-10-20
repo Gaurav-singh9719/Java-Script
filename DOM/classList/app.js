4caaa0b5255b7a78d10e52e12e886d8d34460542
Gaurav Singh <gaurav.singh.971999@gmail.com>
2024-10-20 15:10:51 +0530

    javaScript

---
+++ DOM/classList/app.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("h1");
+// h1.classList.add("naya");
+
+h1.classList.add("naya","special", "purana");
+
+h1.classList.remove("naya");
+h1.classList.toggle("naya");
+
+
+
+
